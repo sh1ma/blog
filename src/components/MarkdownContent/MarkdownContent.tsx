59878549b09ede8e36303f4144ddc99dadf53cc9
sh1ma <in9lude@gmail.com>
2023-11-10 15:10:23 +0900

    feat: 見づらいheadingを修正した

--- src/components/MarkdownContent/MarkdownContent.tsx
+++ src/components/MarkdownContent/MarkdownContent.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from "react"
+import { MDXProvider, useMDXComponents } from "@mdx-js/react"
 
 export const MarkdownContent = ({ children }: React.PropsWithChildren) => {
   return (
@@ -9,7 +10,12 @@ export const MarkdownContent = ({ children }: React.PropsWithChildren) => {
     prose
   prose-li:marker:text-black
   prose-a:text-red-800
-    prose-headings:font-normal"
+    prose-headings:font-bold
+    prose-h2:text-xl
+    prose-h2:border-b-2
+    prose-h2:pb-2
+    prose-h3:text-base
+    "
     >
       {children}
     </div>
