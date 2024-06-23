71defe3d650d75fe088c2e8fe8483f19326111dc
sh1ma <in9lude@gmail.com>
2024-06-23 23:33:51 +0900

    feat: 不要なパッケージ消した

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import type { Metadata } from "next"
 import Link from "next/link"
 import "./globals.scss"
 import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faRssSquare } from "@fortawesome/free-solid-svg-icons"
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://blog.sh1ma.dev/"),
@@ -33,8 +31,7 @@ export default function RootLayout({
                 <Link href="/">
                   <h1 className="my-2 text-3xl">blog.sh1ma.dev</h1>
                 </Link>
-
-                <div>
+                <div className="flex">
                   <div>プログラムが好きです</div>
                 </div>
               </div>
@@ -46,14 +43,26 @@ export default function RootLayout({
             <div>{children}</div>
           </div>
           <footer className="flex flex-col items-center bg-stone-800 py-10 text-stone-50">
-            <div className="flex items-center justify-center gap-4 p-4">
-              <p className="text-sm">
-                <small>{`© ${dayjs().format("YYYY")} sh1ma`}</small>
-              </p>
-              <Link href="/feed">
-                <FontAwesomeIcon width={"24px"} icon={faRssSquare} />
-              </Link>
-            </div>
+            <Link href="/feed">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="mb-2 h-6 w-6"
+                role="img"
+                aria-label="RSSフィードへのリンク"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3.75 4.5a.75.75 0 0 1 .75-.75h.75c8.284 0 15 6.716 15 15v.75a.75.75 0 0 1-.75.75h-.75a.75.75 0 0 1-.75-.75v-.75C18 11.708 12.292 6 5.25 6H4.5a.75.75 0 0 1-.75-.75V4.5Zm0 6.75a.75.75 0 0 1 .75-.75h.75a8.25 8.25 0 0 1 8.25 8.25v.75a.75.75 0 0 1-.75.75H12a.75.75 0 0 1-.75-.75v-.75a6 6 0 0 0-6-6H4.5a.75.75 0 0 1-.75-.75v-.75Zm0 7.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Link>
+            <p className="text-sm">
+              <small>{`© ${dayjs().format("YYYY")} sh1ma`}</small>
+            </p>
+
             <small className="">This website uses Google Analytics</small>
           </footer>
         </div>
