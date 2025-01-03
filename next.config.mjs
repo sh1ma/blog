45a53f88a68f8ea89d889857be2eff901c0f72db
sh1ma <in9lude@gmail.com>
2025-01-03 13:42:22 +0900

    chore: ダメ元でnextのバージョンあげてみた

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev"
+import bundleAnalyzer from "@next/bundle-analyzer"
 import { withContentlayer } from "next-contentlayer2"
 
 // note: the if statement is present because you
@@ -14,8 +15,8 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-// const withBundleAnalyzer = bundleAnalyzer({
-//   enabled: process.env.ANALYZE === "true",
-// })
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})
 
-export default withContentlayer(nextConfig)
+export default withBundleAnalyzer(withContentlayer(nextConfig))
