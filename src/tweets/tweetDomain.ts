57edccabc5909f6afe35739c4d07aa2675cbc316
sh1ma <in9lude@gmail.com>
2025-05-14 00:31:44 +0900

    fix: awaitやめた

--- src/tweets/tweetDomain.ts
+++ src/tweets/tweetDomain.ts
@@ -37,7 +37,7 @@ const recordToModel = (record: TweetDBRecord): Tweet => {
 
 // すべてのツイートを取得する
 export const getAllTweets = async () => {
-  const context = await getCloudflareContext({ async: true })
+  const context = getCloudflareContext()
   const { results } = await context.env.DB.prepare(
     "select * from tweets order by created_at desc",
   ).all<TweetDBRecord>()
@@ -48,7 +48,7 @@ export const getAllTweets = async () => {
 // 最新5件のツイートを取得する
 const RECENT_LIMIT = 5
 export const getRecentTweets = async () => {
-  const context = await getCloudflareContext({ async: true })
+  const context = getCloudflareContext()
   const { results } = await context.env.DB.prepare(
     `select * from tweets order by created_at desc limit ${RECENT_LIMIT}`,
   ).all<TweetDBRecord>()
